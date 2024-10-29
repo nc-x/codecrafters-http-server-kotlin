b8ccf9d38b58fff644e578c15b8e23edd2804c66
Neelesh Chandola <neelesh.chandola@outlook.com>
2024-10-29 19:33:36 +0530

    more request parsing; /user-agent path

--- src/main/kotlin/Request.kt
+++ src/main/kotlin/Request.kt
@@ -7,31 +7,66 @@ enum class Method {
 data class Request(
     val method: Method,
     val path: String,
+    val httpVersion: String,
+    val headers: Map<String, String>
 ) {
     companion object {
         suspend fun parse(reader: ByteReadChannel): Request {
             val requestLine = reader.readUTF8Line()
-            val elems = requestLine!!.split("\\s+".toRegex())
+            val info = requestLine!!.split("\\s+".toRegex())
+
+            val headers = mutableMapOf<String, String>()
+            var headerLine: String
+            while (true) {
+                headerLine = reader.readUTF8Line()!!
+                if (headerLine.isEmpty()) break
+                val (header, value) = headerLine.split(":")
+                headers[header.trim()] = value.trim()
+            }
+
             return Request(
-                method = Method.valueOf(elems[0]),
-                path = elems[1]
+                method = Method.valueOf(info[0]),
+                path = info[1],
+                httpVersion = info[2],
+                headers = headers
             )
         }
 
     }
 }
 
-
 suspend fun handleRequest(request: Request, writer: ByteWriteChannel) {
-    if (request.path == "/") {
-        writer.writeByteArray("HTTP/1.1 200 OK\r\n\r\n".toByteArray())
-    } else if (request.path.startsWith("/echo/")) {
-        val str = request.path.substringAfter("/echo/")
-        writer.writeByteArray("HTTP/1.1 200 OK\r\n".toByteArray())
-        writer.writeByteArray("Content-Type: text/plain\r\n".toByteArray())
-        writer.writeByteArray("Content-Length: ${str.length}\r\n\r\n".toByteArray())
-        writer.writeByteArray(str.toByteArray())
-    } else {
-        writer.writeByteArray("HTTP/1.1 404 Not Found\r\n\r\n".toByteArray())
+    val path = request.path
+    when {
+        path == "/" -> {
+            respond200(writer, null)
+        }
+
+        path.startsWith("/echo/") -> {
+            respond200(writer, path.substringAfter("/echo/"))
+        }
+
+        path == "/user-agent" -> {
+            respond200(writer, request.headers["User-Agent"]!!)
+        }
+
+        else -> {
+            respond404(writer)
+        }
     }
+}
+
+suspend fun respond200(writer: ByteWriteChannel, message: String?) {
+    writer.writeByteArray("HTTP/1.1 200 OK\r\n".toByteArray())
+    if (message == null) {
+        writer.writeByteArray("\r\n".toByteArray())
+        return
+    }
+    writer.writeByteArray("Content-Type: text/plain\r\n".toByteArray())
+    writer.writeByteArray("Content-Length: ${message.length}\r\n\r\n".toByteArray())
+    writer.writeByteArray(message.toByteArray())
+}
+
+suspend fun respond404(writer: ByteWriteChannel) {
+    writer.writeByteArray("HTTP/1.1 404 Not Found\r\n\r\n".toByteArray())
 }
