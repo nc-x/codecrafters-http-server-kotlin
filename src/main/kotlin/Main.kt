2d19a84e13d25b2d6906588b8342b26a55811c9c
Neelesh Chandola <neelesh.chandola@outlook.com>
2024-10-29 18:28:46 +0530

    send 200 OK

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,18 @@
 import io.ktor.network.selector.*
 import io.ktor.network.sockets.*
+import io.ktor.utils.io.*
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
 
-suspend fun main() {
+fun main() = runBlocking {
     val selectorManager = SelectorManager(Dispatchers.IO)
     val server = aSocket(selectorManager)
         .configure { reuseAddress = true }
         .tcp()
         .bind("127.0.0.1", 4221)
 
-    server.accept()
-    println("accepted new connection")
+    server.accept().use {socket ->
+        val writer = socket.openWriteChannel(autoFlush = true)
+        writer.writeByteArray("HTTP/1.1 200 OK\r\n\r\n".toByteArray())
+    }
 }
